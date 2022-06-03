7959897a05aaa00074795d39572d435a4288c15e
uRufus <apdubrov@gmail.com>
2022-06-03 23:26:00 +0300

    Homework 5

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 
+from framework.decorators import UrlDecorator
 from framework.wsgi import Framework
 from framework.url import Url
 from framework.view import View
@@ -9,10 +10,12 @@ from framework.logger import Logger
 
 logger = Logger
 
+
+@UrlDecorator('/', 'Главная')
 class MainPage(View):
 
     def get(self, request):
-        output = render('index.html', object_list='GET SUCCESS', themes_list=urls)
+        output = render('index.html', object_list='GET SUCCESS', themes_list=UrlDecorator.urls)
         logger._log_data('index_file', output)
         return Response(body=output)
 
@@ -20,20 +23,22 @@ class MainPage(View):
         return Response(status='201 Created', body='POST SUCCESS')
 
 
+@UrlDecorator('/about', 'О нас')
 class About(View):
 
     def get(self, request):
-        output = render('about.html', object_list='GET SUCCESS', themes_list=urls)
+        output = render('about.html', object_list='GET SUCCESS', themes_list=UrlDecorator.urls)
         return Response(body=output)
 
     def post(self, request):
         return Response(status='201 Created', body='POST SUCCESS')
 
 
+@UrlDecorator('/contacts', 'Контакты')
 class Contacts(View):
 
     def get(self, request):
-        output = render('contacts.html', object_list='GET SUCCESS', themes_list=urls)
+        output = render('contacts.html', object_list='GET SUCCESS', themes_list=UrlDecorator.urls)
         logger._log_data('contacts', output)
         return Response(body=output)
 
@@ -44,6 +49,7 @@ class Contacts(View):
         return Response(status='201 Created', body='Your message was received')
 
 
+@UrlDecorator('/categories', 'Categories')
 class Categories(View):
 
     def get(self, request):
@@ -51,7 +57,7 @@ class Categories(View):
         with open('framework/file_db/categories', mode='r', encoding='utf-8') as f:
             for value in f.readlines():
                 categories.append(value)
-        output = render('categories.html', object_list=categories, themes_list=urls)
+        output = render('categories.html', object_list=categories, themes_list=UrlDecorator.urls)
         return Response(body=output)
 
     def post(self, request):
@@ -61,12 +67,13 @@ class Categories(View):
         return self.get(self, request)
 
 
+@UrlDecorator('/courses', 'Courses')
 class Courses(View):
 
     def get(self, request):
         with open('framework/file_db/courses.json', mode='r') as f:
             courses = json.load(f)
-        output = render('courses.html', object_list=courses, themes_list=urls)
+        output = render('courses.html', object_list=courses, themes_list=UrlDecorator.urls)
         return Response(body=output)
 
     def post(self, request):
@@ -82,12 +89,12 @@ class Courses(View):
             json.dump(courses, f)
         return self.get(self, request)
 
-urls = [
-    Url('/', MainPage, 'Главная'),
-    Url('/about', About, 'О нас'),
-    Url('/contacts', Contacts, 'Контакты'),
-    Url('/categories', Categories, 'Categories'),
-    Url('/courses', Courses, 'Courses'),
-]
+# urls = [
+#     Url('/', MainPage, 'Главная'),
+#     Url('/about', About, 'О нас'),
+#     Url('/contacts', Contacts, 'Контакты'),
+#     Url('/categories', Categories, 'Categories'),
+#     Url('/courses', Courses, 'Courses'),
+# ]
 
-app = Framework(urls)
+app = Framework(UrlDecorator.urls)
